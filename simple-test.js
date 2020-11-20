bb3ea9470752072d6f7c85d05df65e8b6125210f
LongYinan <lynweklm@gmail.com>
2020-11-20 11:10:05 +0800

    Fix windows i686

--- simple-test.js
+++ simple-test.js
@@ -1,11 +1,9 @@
 const { exec } = require('child_process')
-const { stderr } = require('process')
 
 exec(`node -e "console.log(require('./index.js'))"`, (err, stdout, stderr) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
   console.info(stdout)
   console.info(stderr)
+  if (err) {
+    process.exit(1)
+  }
 })
