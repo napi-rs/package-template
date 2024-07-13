0bfabb2f154508b9025c9e988f888fbb71c7ebe8
LongYinan <lynweklm@gmail.com>
2024-07-14 01:17:59 +0800

    chore: upgrade dependencies (#370)

--- benchmark/bench.ts
+++ benchmark/bench.ts
@@ -1,28 +1,21 @@
-import b from 'benny'
+import { Bench } from 'tinybench'
 
-import { plus100 } from '../index'
+import { plus100 } from '../index.js'
 
 function add(a: number) {
   return a + 100
 }
 
-async function run() {
-  await b.suite(
-    'Add 100',
+const b = new Bench()
 
-    b.add('Native a + 100', () => {
-      plus100(10)
-    }),
-
-    b.add('JavaScript a + 100', () => {
-      add(10)
-    }),
-
-    b.cycle(),
-    b.complete(),
-  )
-}
-
-run().catch((e) => {
-  console.error(e)
+b.add('Native a + 100', () => {
+  plus100(10)
 })
+
+b.add('JavaScript a + 100', () => {
+  add(10)
+})
+
+await b.run()
+
+console.table(b.table())
--- index.js
+++ index.js
@@ -208,12 +208,62 @@ switch (platform) {
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'package-template.linux-arm-gnueabihf.node'))
+        if (isMusl()) {
+          localFileExisted = existsSync(join(__dirname, 'package-template.linux-arm-musleabihf.node'))
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./package-template.linux-arm-musleabihf.node')
+            } else {
+              nativeBinding = require('@napi-rs/package-template-linux-arm-musleabihf')
+            }
+          } catch (e) {
+            loadError = e
+          }
+        } else {
+          localFileExisted = existsSync(join(__dirname, 'package-template.linux-arm-gnueabihf.node'))
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./package-template.linux-arm-gnueabihf.node')
+            } else {
+              nativeBinding = require('@napi-rs/package-template-linux-arm-gnueabihf')
+            }
+          } catch (e) {
+            loadError = e
+          }
+        }
+        break
+      case 'riscv64':
+        if (isMusl()) {
+          localFileExisted = existsSync(join(__dirname, 'package-template.linux-riscv64-musl.node'))
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./package-template.linux-riscv64-musl.node')
+            } else {
+              nativeBinding = require('@napi-rs/package-template-linux-riscv64-musl')
+            }
+          } catch (e) {
+            loadError = e
+          }
+        } else {
+          localFileExisted = existsSync(join(__dirname, 'package-template.linux-riscv64-gnu.node'))
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./package-template.linux-riscv64-gnu.node')
+            } else {
+              nativeBinding = require('@napi-rs/package-template-linux-riscv64-gnu')
+            }
+          } catch (e) {
+            loadError = e
+          }
+        }
+        break
+      case 's390x':
+        localFileExisted = existsSync(join(__dirname, 'package-template.linux-s390x-gnu.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./package-template.linux-arm-gnueabihf.node')
+            nativeBinding = require('./package-template.linux-s390x-gnu.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-linux-arm-gnueabihf')
+            nativeBinding = require('@napi-rs/package-template-linux-s390x-gnu')
           }
         } catch (e) {
           loadError = e
